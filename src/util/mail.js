c2a78924f84006c117d2ca143e776b71dc9c37b3
Donnie West <me@donniewest.com>
2019-07-09 22:58:12 -0500

    Add Mail functions for emailing users

---
+++ src/util/mail.js
@@ -0,0 +1,68 @@
+import fetch from 'node-fetch'
+import { URLSearchParams } from 'url'
+
+const token = process.env.MAILGUN_TOKEN
+const host = process.env.HOST || `http://localhost:${process.env.PORT || 3000}`
+
+if (!token) {
+  throw new Error('MAILGUN_TOKEN env variable not set!')
+}
+
+const mailgunEndpoint = `https://api:${token}@api.mailgun.net/v3/mail.thestandup.app`
+
+function sendEmail (to, subject, body) {
+  const params = new URLSearchParams()
+  params.append('from', 'no-reply@mail.thestandup.app')
+  params.append('to', to)
+  params.append('subject', subject)
+  params.append('text', body)
+
+  return fetch(`${mailgunEndpoint}/messages`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: params
+  })
+}
+
+function sendRegistrationEmail (to, username, token) {
+  const body = `
+    Hi there 👋 ${to},
+
+    You've registered on the Standup App. Here's a link to login for ${username}:
+
+    ${host}?token=${token}
+
+    Salutations,
+    The Standup App team
+`
+
+  const subject = 'Standup App Registration and Verification Code'
+
+  return sendEmail(to, subject, body)
+}
+
+function sendLoginEmail (to, token) {
+  const body = `
+    Hi there 👋 ${to},
+
+    You've requested a login for the Standup App. Here's a link to login:
+
+    ${host}?token=${token}
+
+    Salutations,
+    The Standup App team
+`
+
+  const subject = 'Standup App Verification Code'
+
+  return sendEmail(to, subject, body)
+}
+
+export default {
+  sendLoginEmail,
+  sendRegistrationEmail
+}
+
+export { sendLoginEmail, sendRegistrationEmail }
